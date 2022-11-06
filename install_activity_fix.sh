a13efd8e09e2a8fac75229bb9d1ff87c8deb5e0c
axofhell <osmankahramann@gmail.com>
2022-11-07 01:58:57 +0300

    Update install_activity_fix.sh

--- install_activity_fix.sh
+++ install_activity_fix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo wget -O /root/activity_fix.sh https://raw.githubusercontent.com/axofhell/helium_mining_activity_fix/main/activity_fix.sh; chmod a+x /root/activity_fix.sh
-{ crontab -l; echo "0 * * * * sh /root/activity_fix.sh"; } | crontab -
+{ crontab -l; echo "30 * * * * sh /root/activity_fix.sh"; } | crontab -
